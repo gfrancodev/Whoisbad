e545a39e55cc51508dfc71753c03287a09acc02b
gfrancodev <guatavojorgee2511@gmail.com>
2021-04-12 10:06:42 -0300

    [START]

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM php:8.0-apache
+RUN apt-get update && apt-get install -y --no-install-recommends netbase
+RUN apt-get update && apt-get install -y whois
 RUN docker-php-ext-install mysqli
--- www/index.php
+++ www/index.php
@@ -1,19 +1,8 @@
 <?php
-header("Content-Type: application/json; charset=UTF-8");
-header("response_code: 200");
-header("X-Powered-By: gfrancodev");
 
-//$server = $_SERVER["HTTP_HOST"];
 
-file_get_contents("http://www.google.com");
-echo json_encode(var_dump($http_response_header));
-
-$whois = shell_exec("whois.exe -v wilson.com.br > wilson.txt");
+$whois = shell_exec("whois gustavo.com.br");
 echo $whois;
-$data = explode("\n", $whois);
 
-echo json_encode([
-    "total" => count($data)
-]);
 
 
