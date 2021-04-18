bed0f4687a7c7822dcdf6e36a98d5b097133f84f
gfrancodev <guatavojorgee2511@gmail.com>
2021-04-18 12:17:57 -0300

    [START]

--- App/Daring/providers/IwhoisProvider.php
+++ App/Daring/providers/IwhoisProvider.php
@@ -3,6 +3,5 @@ namespace Whoisbad\Daring\providers;
 
 interface IwhoisProvider
 {
-    public static function filter(string $input, string $output, string $content): string;
     public function fetch(string $site): string;
 }
